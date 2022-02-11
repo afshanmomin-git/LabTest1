d3fe6f74128c9b614e45dd2ca8f24e8361e8c4f9
“Afshan <afshanmomin123@gmail.com>
2022-02-11 15:24:03 -0500

    Initial Commit

---
+++ LabTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabTest1
+//
+//  Created by Afshan M on 2022-02-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LabTest1Tests/LabTest1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  LabTest1Tests.swift
+//  LabTest1Tests
+//
+//  Created by Afshan M on 2022-02-11.
+//
+
+import XCTest
+@testable import LabTest1
+
+class LabTest1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
